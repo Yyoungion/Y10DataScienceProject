6dced21e67d9be87bf26ad21c64f7af5f0a07779
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-03-19 10:08:11 +1100

    I tried to play music. Failed

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
-from playsound import playsound
-import requests, pygame, sys, time
-pygame.mixer.init()
-y = "ditto"
-API_URL = "https://pokeapi.co/api/v2/pokemon/{y}"
-x = requests.get("https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/132.ogg")
-pygame.mixer.Sound(x)
+import requests
+import pygame
+import time
+import threading
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    url = f"{base_url}/pokemon/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemon_data = response.json()
+        return pokemon_data
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+        
+        
+pokemon_name = "ditto"
+pokemon_info = get_pokemon_info(pokemon_name)
+
+if pokemon_info:
+    a = f"{pokemon_info["cries"]["latest"]  }"
+    b = requests.get(a)
+    
+doc = requests.get(b)
+f = open("movie.mp3","wb")
+f.write(doc.text)
+f.close()
+    
+    
+    
+def playmusic():       
+    def play_music(b):
+        pygame.mixer.init()
+        pygame.mixer.music.load(b)
+        pygame.mixer.play_music()
+    
+    def wait_for_input():
+        input()
+        pygame.mixer.music.stop()
+  
+
+    music_thread = threading.Thread(target=play_music, args=(b))
+    input_thread = threading.Thread(target=wait_for_input)
+
+    music_thread.start()
+    input_thread.start()
