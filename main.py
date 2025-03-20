dc15661900f03c648b10a76656a58cbde0e18979
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-03-21 08:34:51 +1100

    Added more to the program. I am trying to make the mp3 file be able to play sound

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import requests
 import pygame
 import time
 import threading
+from pathlib import Path
+import random
+import os
+
+pygame.init()
+pygame.mixer.init()
+
 
 base_url = "https://pokeapi.co/api/v2/"
 
@@ -14,13 +21,67 @@ def get_pokemon_info(name):
         return pokemon_data
     else:
         print(f"Failed to retrieve data {response.status_code}")
-        
-        
-pokemon_name = "ditto"
-pokemon_info = get_pokemon_info(pokemon_name)
 
-if pokemon_info:
-    a = f"{pokemon_info['cries']['latest']}"
-    print(a)
+def download_all_pokemon():
+    print("Do you want to have all the pokemon sound files? ")
+    print("This will remove the need to download a pokemon call every time you want to play a sound from a new pokemon.")
+    print("This may take a long time.")
+    download = input("Y/N ")
+
+    if download == "Y":
+        print("Downloading")
+        print("Estimated time: 20 minutes")
+        for i in range(1,1026):
+            pokemon_info = get_pokemon_info(i)
+            a = f"{i:04}_{pokemon_info['forms'][0]['name']}.latest"
+
+            out_file = Path(f"~/Documents/Github\Y10DataScienceProject\SoundStorageALL\{a.capitalize()}.mp3").expanduser()
+            resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{i}.ogg")
+            resp.raise_for_status()
+            with open(out_file, "wb") as fout:
+                fout.write(resp.content)
+    elif download == "N":
+        pass
+    else:
+        playrandomsound()
+
+def playrandomsound():
+    for i in range (0,10):
+        random_pokemon = random.randint(1, 1025)
+        pokemon_info = get_pokemon_info(random_pokemon)
+        a = f"{random_pokemon:04}_{pokemon_info['forms'][0]['name']}.latest"
+
+        out_file = Path(f"~/Documents/Github\Y10DataScienceProject\RandomSound\{a.capitalize()}.mp3").expanduser()
+        resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{random_pokemon}.ogg")
+        resp.raise_for_status()
+        with open(out_file, "wb") as fout:
+            fout.write(resp.content)
+        time.sleep(1)
+        
+    lists_of_songs = os.listdir(r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\')
+
+    for song in lists_of_songs:
+        if song.endswith(".mp3"):
+            file_path = r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\' + song
+            pygame.mixer.music.load(str(file_path))
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy() == True:
+                continue
+
+
+
+def main():
+    #download_all_pokemon()
+    playrandomsound()
+
+main()
+
+
+
+
+
+
+
+
     
 
