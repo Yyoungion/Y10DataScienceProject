f17c58fbb055f97d0ece93198d5d327a7effa687
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-03-21 13:28:44 +1100

    It can play sound now. I need to delet it so the user can re run it.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import os
 pygame.init()
 pygame.mixer.init()
 
-
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(name):
@@ -51,27 +50,27 @@ def playrandomsound():
         pokemon_info = get_pokemon_info(random_pokemon)
         a = f"{random_pokemon:04}_{pokemon_info['forms'][0]['name']}.latest"
 
-        out_file = Path(f"~/Documents/Github\Y10DataScienceProject\RandomSound\{a.capitalize()}.mp3").expanduser()
+        out_file = Path(f"~/Documents/Github\Y10DataScienceProject\RandomSound\{a.capitalize()}.ogg").expanduser()
         resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{random_pokemon}.ogg")
         resp.raise_for_status()
         with open(out_file, "wb") as fout:
             fout.write(resp.content)
-        time.sleep(1)
-        
+
+    time.sleep(4)
+
     lists_of_songs = os.listdir(r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\')
 
     for song in lists_of_songs:
-        if song.endswith(".mp3"):
+        if song.endswith(".ogg"):
             file_path = r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\' + song
             pygame.mixer.music.load(str(file_path))
             pygame.mixer.music.play()
             while pygame.mixer.music.get_busy() == True:
                 continue
-
-
+    time.sleep(20)
 
 def main():
-    #download_all_pokemon()
+    download_all_pokemon()
     playrandomsound()
 
 main()
