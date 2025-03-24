21affdf3fae05e78b6f9671588dc7c5d922dbff3
Yyoungion <Yi.du8@education.nsw.gov.au>
2025-03-24 11:09:41 +1100

    Finished my code. It works except the capitalize.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import pygame
 import time
-import threading
 from pathlib import Path
 import random
 import os
@@ -12,14 +11,18 @@ pygame.mixer.init()
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
-    response = requests.get(url)
+    try:
+        url = f"{base_url}/pokemon/{name}"
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        pokemon_data = response.json()
-        return pokemon_data
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
+        if response.status_code == 200:
+            pokemon_data = response.json()
+            return pokemon_data
+        else:
+            print(f"Failed to retrieve data {response.status_code}")
+            
+    except:
+        pass
 
 def download_all_pokemon():
     print("Do you want to have all the pokemon sound files? ")
@@ -32,7 +35,7 @@ def download_all_pokemon():
         print("Estimated time: 20 minutes")
         for i in range(1,1026):
             pokemon_info = get_pokemon_info(i)
-            a = f"{i:04}_{pokemon_info['forms'][0]['name']}.latest"
+            a = f"{str(i).zfill(4)}_{pokemon_info['forms'][0]['name']}.latest"
 
             out_file = Path(f"~/Documents/Github\Y10DataScienceProject\SoundStorageALL\{a.capitalize()}.mp3").expanduser()
             resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{i}.ogg")
@@ -45,33 +48,119 @@ def download_all_pokemon():
         playrandomsound()
 
 def playrandomsound():
-    for i in range (0,10):
-        random_pokemon = random.randint(1, 1025)
-        pokemon_info = get_pokemon_info(random_pokemon)
-        a = f"{random_pokemon:04}_{pokemon_info['forms'][0]['name']}.latest"
+    try:
+        for i in range (0,1):
+            random_pokemon = random.randint(1, 1025)
+            pokemon_info = get_pokemon_info(random_pokemon)
+            a = f"{str(random_pokemon).zfill(4)}_{pokemon_info['forms'][0]['name']}.latest"
 
-        out_file = Path(f"~/Documents/Github\Y10DataScienceProject\RandomSound\{a.capitalize()}.ogg").expanduser()
-        resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{random_pokemon}.ogg")
+            out_file = Path(f"RandomSound\{a.capitalize()}.ogg").expanduser()
+            resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{random_pokemon}.ogg")
+            resp.raise_for_status()
+            with open(out_file, "wb") as fout:
+                fout.write(resp.content)
+            time.sleep(1)
+            
+        lists_of_songs = os.listdir(r"RandomSound"'\\')
+
+        for song in lists_of_songs:
+            if song.endswith(".ogg"):
+                file_path = r"RandomSound"'\\' + song
+                pygame.mixer.music.load(str(file_path))
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy() == True:
+                    continue
+    except:
+        pass
+
+def playsound():
+    try:
+        poke = input("What pokemon do you want to hear? ")
+        pokemon_info = get_pokemon_info(poke)
+        pokeID = f"{pokemon_info['id']}"
+        pokesound = f"{str(pokeID).zfill(4)}_{pokemon_info['forms'][0]['name']}.latest"
+        out_file = Path(f"SoundStorage\{pokesound.capitalize()}.ogg").expanduser()
+        resp = requests.get(f"https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/{pokeID}.ogg")
         resp.raise_for_status()
         with open(out_file, "wb") as fout:
             fout.write(resp.content)
         time.sleep(1)
+        file_path = r"SoundStorage"'\\' + pokesound + ".ogg"
+        pygame.mixer.music.load(str(file_path))
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
+        print("This sound has been automatically saved (If it hasn't been already).")
+    except:
+        pass
+
+def playallstoredsound():
+    try:
+        list_of_songs = os.listdir(r"SoundStorage"'\\')
         
-    lists_of_songs = os.listdir(r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\')
-
-    for song in lists_of_songs:
-        if song.endswith(".mp3"):
-            file_path = r"c:\Users\Yyoung Du\Documents\Github\Y10DataScienceProject\RandomSound"'\\' + song
-            pygame.mixer.music.load(str(file_path))
-            pygame.mixer.music.play()
-            while pygame.mixer.music.get_busy() == True:
-                continue
-    time.sleep(20)
-
+        for song in list_of_songs:
+            if song.endswith(".ogg"):
+                file_path = r"SoundStorage"'\\' + song
+                pygame.mixer.music.load(str(file_path))
+                pygame.mixer.music.play()
+                while pygame.mixer.music.get_busy() == True:
+                    continue
+    except:
+        pass
+    
+def viewstoresound():
+    try:
+        print("Here is all the pokemon sounds that have been stored.")
+        list_of_songs = os.listdir(r"SoundStorage"'\\')
+        for song in list_of_songs:
+            if song.endswith(".latest.ogg"):
+                song = song.replace(song[0:5],"",1)
+                song = song.replace(".latest.ogg",'')
+                songcap = song.capitalize()
+                print(songcap)
+    except:
+        pass
+    
 def main():
-    download_all_pokemon()
-    playrandomsound()
-
+    try:
+        while True:
+            print("To hear a pokemon sound input 1")
+            print("To view stored pokemon, input 2")
+            print("To play all of the stored pokemon. input 3")
+            print("To quit, input 4")
+            choice = int(input("What do you want to do (1 - 4): "))
+            
+            if choice == 1:
+                playsound()
+                time.sleep(3)
+                print("")
+                print("")
+            elif choice == 2:
+                viewstoresound()
+                time.sleep(3)
+                print("")
+                print("")
+            elif choice == 3:
+                playallstoredsound()
+                time.sleep(3)
+                print("")
+                print("")
+            elif choice == 4:
+                exit
+                quit
+                break
+            else:
+                print("ERROR - U stupid Please try again")
+                playrandomsound()
+                time.sleep(3)
+                print("")
+                print("")
+    except:
+        print("Error. Try again")
+        print("")
+        print("")
+        main()
+    
 main()
 
 
